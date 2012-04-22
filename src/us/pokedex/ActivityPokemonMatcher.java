44fe78835e20cf1cee8c0b61c827ca9d13eb2510
Sean Rossi <srossi455@gmail.com>
2012-04-22 16:05:38 -0700

    Created Pokemon class

---
+++ src/us/pokedex/ActivityPokemonMatcher.java
@@ -0,0 +1,15 @@
+package us.pokedex;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityPokemonMatcher extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pokemon_matcher);
+    }
+    
+    private 
+}
