7852ff8926f1a46ea40b2c7b66debd044d7edea9
Sean Rossi <srossi455@gmail.com>
2012-05-08 16:23:54 -0700

    Implemented main menu, added Launcher Icon

--- gen/us/pokedex/R.java
+++ gen/us/pokedex/R.java
@@ -12,17 +12,35 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int icon_art=0x7f020001;
+        public static final int icon_comedy=0x7f020002;
+        public static final int icon_esp=0x7f020003;
+        public static final int icon_heart=0x7f020004;
+        public static final int icon_outdoors=0x7f020005;
+        public static final int icon_sports=0x7f020006;
+        public static final int icon_swimming=0x7f020007;
+        public static final int icon_videogame=0x7f020008;
     }
     public static final class id {
+        public static final int ImageButton01=0x7f05000d;
+        public static final int ImageButton02=0x7f05000c;
+        public static final int ImageButton03=0x7f05000b;
+        public static final int ImageButton04=0x7f050007;
+        public static final int ImageButton05=0x7f050008;
+        public static final int ImageButton06=0x7f050009;
+        public static final int ImageButton07=0x7f05000a;
         public static final int button1=0x7f050002;
-        public static final int button2=0x7f050006;
-        public static final int button3=0x7f050008;
-        public static final int editText1=0x7f050001;
-        public static final int imageView1=0x7f050004;
-        public static final int imgFrame=0x7f050003;
-        public static final int listView1=0x7f050005;
-        public static final int progressBar1=0x7f050007;
+        public static final int button2=0x7f050003;
+        public static final int button3=0x7f050004;
+        public static final int button4=0x7f050005;
+        public static final int editText1=0x7f05000e;
+        public static final int imageButton1=0x7f050006;
+        public static final int imageView1=0x7f050010;
+        public static final int imgFrame=0x7f05000f;
+        public static final int listView1=0x7f050011;
+        public static final int progressBar1=0x7f050012;
         public static final int relativeLayout1=0x7f050000;
+        public static final int relativeLayout2=0x7f050001;
     }
     public static final class layout {
         public static final int activity_pokedex_main_menu=0x7f030000;
@@ -30,6 +48,7 @@ public final class R {
         public static final int activity_search_pokemon=0x7f030002;
         public static final int activity_show_results=0x7f030003;
         public static final int dialog_options=0x7f030004;
+        public static final int dialog_save_image_to_phone=0x7f030005;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
---
+++ src/us/pokedex/ActivityPokemonRandom.java
@@ -0,0 +1,5 @@
+package us.pokedex;
+
+public class ActivityPokemonRandom {
+
+}
