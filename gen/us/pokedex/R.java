c6708bc702f01a1c15134ffd0b9654f09c28f655
Pia Cali <peeyuuh@gmail.com>
2012-05-08 15:44:25 -0700

    more gui updates

--- gen/us/pokedex/R.java
+++ gen/us/pokedex/R.java
@@ -16,6 +16,7 @@ public final class R {
     public static final class id {
         public static final int button1=0x7f050002;
         public static final int button2=0x7f050006;
+        public static final int button3=0x7f050008;
         public static final int editText1=0x7f050001;
         public static final int imageView1=0x7f050004;
         public static final int imgFrame=0x7f050003;
@@ -32,6 +33,9 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int back=0x7f040002;
         public static final int hello=0x7f040000;
+        public static final int play=0x7f040004;
+        public static final int search=0x7f040003;
     }
 }
--- src/us/pokedex/ActivityShowSearchResults.java
+++ src/us/pokedex/ActivityShowSearchResults.java
@@ -3,11 +3,11 @@ package us.pokedex;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class ActivityShowResults extends Activity {
+public class ActivityShowSearchResults extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_show_results);
+        setContentView(R.layout.activity_search_pokemon);
     }
 }
