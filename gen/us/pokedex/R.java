53d43ae1ec6b8a1e237b692f3d7dd0b5f1e3e0e5
Pia Cali <peeyuuh@gmail.com>
2012-05-08 14:54:41 -0700

    gui work on displaying results.

--- gen/us/pokedex/R.java
+++ gen/us/pokedex/R.java
@@ -13,12 +13,22 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int button1=0x7f050002;
+        public static final int button2=0x7f050006;
+        public static final int editText1=0x7f050001;
+        public static final int imageView1=0x7f050004;
+        public static final int imgFrame=0x7f050003;
+        public static final int listView1=0x7f050005;
+        public static final int progressBar1=0x7f050007;
+        public static final int relativeLayout1=0x7f050000;
+    }
     public static final class layout {
         public static final int activity_pokedex_main_menu=0x7f030000;
         public static final int activity_pokemon_matcher=0x7f030001;
         public static final int activity_search_pokemon=0x7f030002;
         public static final int activity_show_results=0x7f030003;
-        public static final int dialog_save_image_to_phone=0x7f030004;
+        public static final int dialog_options=0x7f030004;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
