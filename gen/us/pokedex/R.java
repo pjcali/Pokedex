eb3b27aba2543d41a823707d7553ca81855b9c5a
Pia Cali <peeyuuh@gmail.com>
2012-05-08 18:41:50 -0700

    connecting everything in UI

--- gen/us/pokedex/R.java
+++ gen/us/pokedex/R.java
@@ -29,18 +29,18 @@ public final class R {
         public static final int ImageButton05=0x7f050008;
         public static final int ImageButton06=0x7f050009;
         public static final int ImageButton07=0x7f05000a;
-        public static final int button1=0x7f050002;
-        public static final int button2=0x7f050003;
-        public static final int button3=0x7f050004;
-        public static final int button4=0x7f050005;
+        public static final int button1=0x7f050001;
+        public static final int button2=0x7f050002;
+        public static final int button3=0x7f050003;
+        public static final int button4=0x7f050004;
         public static final int editText1=0x7f05000e;
         public static final int imageButton1=0x7f050006;
         public static final int imageView1=0x7f050010;
         public static final int imgFrame=0x7f05000f;
         public static final int listView1=0x7f050011;
         public static final int progressBar1=0x7f050012;
-        public static final int relativeLayout1=0x7f050000;
-        public static final int relativeLayout2=0x7f050001;
+        public static final int relativeLayout1=0x7f050005;
+        public static final int relativeLayout2=0x7f050000;
     }
     public static final class layout {
         public static final int activity_pokedex_main_menu=0x7f030000;
@@ -53,6 +53,10 @@ public final class R {
         public static final int app_name=0x7f040001;
         public static final int back=0x7f040002;
         public static final int hello=0x7f040000;
+        public static final int pkmnmatch=0x7f040005;
+        public static final int pkmnoptions=0x7f040008;
+        public static final int pkmnrand=0x7f040007;
+        public static final int pkmnsearch=0x7f040006;
         public static final int play=0x7f040004;
         public static final int search=0x7f040003;
     }
--- src/us/pokedex/ActivityPokedexMainMenu.java
+++ src/us/pokedex/ActivityPokedexMainMenu.java
@@ -1,12 +1,15 @@
 package us.pokedex;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class ActivityPokedexMainMenu extends Activity {
     /** Called when the activity is first created. */
+	Context c = this;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,22 +17,24 @@ public class ActivityPokedexMainMenu extends Activity {
     }
     
     public void startPokemonMatcher(View v){
-    	startActivity(new Intent(this, ActivityPokemonMatcher.class));
-    	finish();
+    	Log.i("PIADEBUG", "JUST GOT INSIDE THE START function.");
+    	startActivity(new Intent(c, ActivityPokemonMatcher.class));
+    	//finish();
     }
     
-    void startPokemonLookup(View v){
-    	startActivity(new Intent(this, ActivityPokemonMatcher.class));
-    	finish();
+    public void startSearchPokemon(View v){
+    	Log.i("PIADEBUG", "JUST GOT INSIDE THE START function.");
+    	startActivity(new Intent(c, ActivitySearchPokemon.class));
+    	//finish();
     }
     
-    void startPokemonRandom(View v){
-    	startActivity(new Intent(this, ActivityPokemonMatcher.class));
-    	finish();
+    public void startPokemonRandom(View v){
+    	startActivity(new Intent(c, ActivityPokemonRandom.class));
+    	//finish();
     }
     
-    void startOptions(View v){
-    	startActivity(new Intent(this, ActivityPokemonMatcher.class));
-    	finish();
+    public void startOptions(View v){
+    	startActivity(new Intent(c, ActivityPokemonMatcher.class));
+    	//finish();
     }
 }
