bb722d28c0abeba658a18c9a12a63ea85f1dd535
pjcali <peeyuuh@gmail.com>
2012-05-16 15:44:13 -0700

    FIXING

--- gen/us/pokedex/R.java
+++ gen/us/pokedex/R.java
@@ -11,7 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-<<<<<<< HEAD
+
         public static final int android_logo_pixelated=0x7f020000;
         public static final int android_pressed=0x7f020001;
         public static final int button_options=0x7f020002;
@@ -92,7 +92,7 @@ public final class R {
         public static final int relativeLayout2=0x7f050004;
         public static final int shape=0x7f05001f;
         public static final int space1=0x7f050000;
-=======
+
         public static final int ic_launcher=0x7f020000;
         public static final int icon_art=0x7f020001;
         public static final int icon_blue=0x7f020002;
@@ -168,7 +168,7 @@ public final class R {
         public static final int tvWeight=0x7f050029;
         public static final int tvspAttack=0x7f050025;
         public static final int tvspDefense=0x7f050028;
->>>>>>> 7866690c5dec066dea92cf40ea0f11f84b272226
+
     }
     public static final class layout {
         public static final int activity_pokedex_main_menu=0x7f030000;
@@ -176,12 +176,12 @@ public final class R {
         public static final int activity_search_pokemon=0x7f030002;
         public static final int activity_show_results=0x7f030003;
         public static final int dialog_options=0x7f030004;
-<<<<<<< HEAD
+
         public static final int test=0x7f030005;
-=======
+
         public static final int dialog_pokemon_info=0x7f030005;
         public static final int display_results=0x7f030006;
->>>>>>> 7866690c5dec066dea92cf40ea0f11f84b272226
+
     }
     public static final class string {
         public static final int app_name=0x7f040001;
